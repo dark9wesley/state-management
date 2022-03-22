dd4d7d8bb5217ba8726bc1db8589ab20e3245b38
pengyw97 <877285030@qq.com>
2022-03-22 16:42:05 +0800

    feat: TodoFooter 数据展示

--- src/components/TodoFooter.tsx
+++ src/components/TodoFooter.tsx
@@ -1,15 +1,29 @@
-const TodoFooter: React.FC = () => {
+import { connect } from "react-redux";
+
+interface TodoFooterProps {
+  completedNum: number,
+  totalNum: number,
+}
+
+const TodoFooter: React.FC<TodoFooterProps> = ({ completedNum, totalNum }) => {
   return (
     <div className="todo-footer">
       <label>
         <input type="checkbox"/>
       </label>
       <span>
-        <span>已完成0</span> / 全部0
+        <span>已完成{completedNum}</span> / 全部{totalNum}
       </span>
       <button className="btn btn-danger">清除已完成任务</button>
     </div>
   );
 }
+
+const mapStateToProps = (state) => {
+  return {
+    completedNum: state.todoList.filter(todo => todo.completed).length,
+    totalNum: state.todoList.length,
+  }
+}
  
-export default TodoFooter;
+export default connect(mapStateToProps)(TodoFooter)
